0492865d9d5ab5512d3bb2db9fe65569359103d4
Domenick Packett <Domenick.Packett@gmail.com>
2020-02-11 18:14:37 -0500

    fix: api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: true,
   test: false,
   i18nPrefix: '/relovely',
-  apiUrl: 'http://167.172.133.31',
+  apiUrl: 'https://159.203.68.161',
   instagramAuthUrl: `https://api.instagram.com/oauth/authorize?scope=user_profile,user_media&response_type=code`,
   instagramAccessTokenUrl: 'https://api.instagram.com/oauth/access_token',
   instagramGraphUrl: 'https://graph.instagram.com',
