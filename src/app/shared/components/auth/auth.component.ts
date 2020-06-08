3475136d4d3d9df789a0a1724166ba13ef6c04a2
Domenick Packett <Domenick.Packett@gmail.com>
2020-06-08 08:24:59 -0400

    fix: json

--- src/app/shared/components/auth/auth.component.ts
+++ src/app/shared/components/auth/auth.component.ts
@@ -74,13 +74,15 @@ export class AuthComponent implements OnChanges {
 
   signUpInstagram() {
     this.loading = true;
-    const url = this.signupInstagramUrl + `?state={ "email": "${this.sellerForm.get('email').value}", "type: member}"`;
+    const state = JSON.stringify({ email: this.sellerForm.get('email').value, type: 'member' });
+    const url = this.signupInstagramUrl + `?state=${state}`;
     location.replace(url);
   }
 
   sellWithInstagram() {
     this.loading = true;
-    const url = this.signupInstagramUrl + `?state={ "email": "${this.sellerForm.get('email').value}", "type: member}"`;
+    const state = JSON.stringify({ email: this.sellerForm.get('email').value, type: 'seller' });
+    const url = this.signupInstagramUrl + `?state=${state}`;
     location.replace(url);
   }
 
