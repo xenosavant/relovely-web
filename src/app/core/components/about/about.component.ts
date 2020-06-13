51b7e535c8a5f090152b30c0c9c8d0c955ed8d7d
Domenick Packett <Domenick.Packett@gmail.com>
2020-06-13 16:49:49 -0400

    fix: about page

--- src/app/core/components/about/about.component.ts
+++ src/app/core/components/about/about.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['../home/home.component.scss'],
+  styleUrls: ['./about.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  mobile: boolean;
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit() {
+    this.breakpointObserver.observe(['(max-width: 899px)']).subscribe(result => {
+      this.mobile = result.matches;
+    })
   }
 
 }
