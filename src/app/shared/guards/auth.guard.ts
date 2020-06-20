59c58c0a25f7a1d6dfdf1a7e3920ec70dd321948
Domenick Packett <Domenick.Packett@gmail.com>
2020-06-19 21:24:22 -0400

    fix: open auth window

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -6,14 +6,16 @@ import { map } from 'rxjs/operators';
 import { AuthenticationService, IAuthentication } from '../services/authentication.service';
 import { AuthService } from '@app/shared/services/auth/auth.service';
 import { UserService } from '@app/shared/services/user/user.service';
+import { NavigationService } from '../services/navigation/navigation.service';
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate, CanActivateChild {
-    constructor(private userService: UserService) { }
+    constructor(private userService: UserService, private navigationService: NavigationService) { }
 
     canActivate(): Observable<boolean> {
         return this.userService.loggedIn$.pipe(
             map((loggedIn: boolean) => {
+                this.navigationService.openAuthWindow({ page: 'signin' });
                 return loggedIn;
             })
         );
