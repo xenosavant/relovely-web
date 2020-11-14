2a6855cee4f1d5683e2361f03b236189f1781cd3
Domenick Packett <Domenick.Packett@gmail.com>
2020-11-14 12:45:05 -0500

    fix: only log errors in production

--- src/main.ts
+++ src/main.ts
@@ -7,21 +7,20 @@ import * as Sentry from "@sentry/angular";
 import { Integrations } from "@sentry/tracing";
 
 
-Sentry.init({
-  dsn: "https://7dc3c4b511db4c71b80a1dda2e761d86@o476731.ingest.sentry.io/5516789",
-  integrations: [
-    new Integrations.BrowserTracing({
-      tracingOrigins: ["localhost", "https://relovely.com"],
-      routingInstrumentation: Sentry.routingInstrumentation,
-    }),
-  ],
-
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
-  tracesSampleRate: 1.0,
-});
-
 if (environment.production) {
+  Sentry.init({
+    dsn: "https://7dc3c4b511db4c71b80a1dda2e761d86@o476731.ingest.sentry.io/5516789",
+    integrations: [
+      new Integrations.BrowserTracing({
+        tracingOrigins: ["localhost", "https://relovely.com"],
+        routingInstrumentation: Sentry.routingInstrumentation,
+      }),
+    ],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+  });
   enableProdMode();
 }
 
